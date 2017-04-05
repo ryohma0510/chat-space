c6e26a9fa6f2eda359496370c809b14c926e8c49
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-05 09:46:45 +0800

    create group by using strong parameter

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,14 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+    Group.create(group_params)
+    redirect_to controller: 'comments', action: 'index'
+  end
+
+  private
+  def group_params
+    params.require(:chat_group).permit(:name)
+  end
 end
