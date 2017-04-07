b5127c177beaae7065a4b342d27dcd9320507699
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-07 21:53:38 +0900

    add reject_if method

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,5 @@ class Group < ApplicationRecord
   #association
   has_many :users_groups
   has_many :users, through: :users_groups
-  accepts_nested_attributes_for :users_groups
+  accepts_nested_attributes_for :users_groups, reject_if: proc { |attributes| attributes['user_id'].blank? }
 end
