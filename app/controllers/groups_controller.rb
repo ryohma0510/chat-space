24247c3b0c1f5825a2b44f6d1bfe1d9cf43d0640
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-08 15:53:17 +0900

    delete group_id param
    these description was unnecessary when using
    “accepts_nested_attributes_for”.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,10 +17,10 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    selected_params = params.require(:group).permit(:name, :group_id, user_ids:[])
+    selected_params = params.require(:group).permit(:name, user_ids:[])
     users_groups_array = []
     selected_params[:user_ids].each do |id|
-      users_groups_array << { user_id: id, group_id: selected_params[:group_id] }
+      users_groups_array << { user_id: id }
     end
     return { name: "#{selected_params[:name]}", users_groups_attributes: users_groups_array }
   end
