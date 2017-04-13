3ff8faffc21519c9608455682e5446af7e0a0d2d
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-13 20:34:50 +0900

    modify waste code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,6 @@
 class MessagesController < ApplicationController
   def create
-    message = Message.new(message_params)
-    if message.save
+    if current_user.messages.new(message_params).save
       redirect_to group_path(message_params[:group_id])
     else
       redirect_to group_path(message_params[:group_id]), alert: 'メッセージが入力されていません'
@@ -11,6 +10,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :group_id).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :group_id)
   end
 end
