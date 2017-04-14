bb950b2fc42b96d03917113cd2a29c2778ff756f
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-14 09:42:24 +0900

    don't use %description

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for  :users
   root        'groups#index'
-  resources   :groups, only: %i(new create show edit update)
-  resources   :messages, only: %i(create)
+  resources   :groups, only: [:new, :create, :show, :edit, :update]
+  resources   :messages, only: [:create]
 end
