47e300853fb25093695a9510d5e842677b835ecc
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-15 13:51:01 +0900

    test

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe Message do
+  describe '#create' do
+
+    it "is valid with a content" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    it "is invalid when content is nil" do
+      message = build(:message, content: '')
+      message.valid?
+      expect(message.errors[:content]).to include("を入力してください。")
+    end
+  end
+end
