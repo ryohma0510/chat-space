f693173830dc90edaacfe642418af8ce67965171
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-17 18:46:06 +0900

    test code
    use ‘let’

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
 
-    it "is valid with a content" do
-      message = build(:message)
+    let(:user)  { create(:user) }
+    let(:group) { create(:group) }
+
+    it "is valid with a content, user_id, group_id && pass the foreign_key constraint" do
+      message = create(:message, user_id: user.id, group_id: group.id)
       expect(message).to be_valid
     end
 
@@ -13,5 +16,18 @@ describe Message do
       message.valid?
       expect(message.errors[:content]).to include("を入力してください。")
     end
+
+    it "is invalid when user_id is nil" do
+      message = build(:message, user_id: '')
+      message.valid?
+      expect(message.errors[:user_id]).to include("を入力してください。")
+    end
+
+    it "is invalid when group_id is nil" do
+      message = build(:message, group_id: '')
+      message.valid?
+      expect(message.errors[:group_id]).to include("を入力してください。")
+    end
+
   end
 end
