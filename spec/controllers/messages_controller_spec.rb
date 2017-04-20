e5fc881e5a682d95becac3734eabadb5217916c6
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-20 19:00:11 +0900

    test of create action of messages_controller

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,3 +1,29 @@
+require 'rails_helper'
+
+describe MessagesController, type: :controller do
+
+  let(:user)    { create(:user) }
+  let(:group)   { create(:group) }
+  let(:message) { { message: attributes_for(:message, user_id: user.id, group_id: group.id) } }
+
   before do
     login_user user
   end
+
+  describe 'POST #create' do
+    it 'redirects groups#show when a content of message exists' do
+      post :create, message
+      expect(response).to redirect_to group_path(group.id)
+    end
+
+    it 'redirects groups#show when a content of message is nil' do
+      post :create, { message: attributes_for(:message,content: '', user_id: user.id, group_id: group.id) }
+      expect(response).to redirect_to group_path(group.id)
+    end
+
+    it 'alerts when a content of message is nil' do
+     post :create, { message: attributes_for(:message,content: '', user_id: user.id, group_id: group.id) }
+     expect(flash[:alert]).to include('メッセージが入力されていません')
+    end
+  end
+end
