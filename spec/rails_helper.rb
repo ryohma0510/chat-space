4a790d701beb32b347b0c81bb57bc667a8f21599
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-20 18:09:17 +0900

    settings for 'rails-controller-testing'

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,4 +50,10 @@ RSpec.configure do |config|
 
   #factory_girls
     config.include FactoryGirl::Syntax::Methods
+  #settings for gem 'rails-controller-testing'
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
 end
