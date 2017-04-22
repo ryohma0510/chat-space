f5560631a4de8d7b2165eb6894fe3b60c6833872
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-22 15:48:06 +0900

    Ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
   def create
-    if current_user.messages.new(message_params).save
-      redirect_to group_path(message_params[:group_id])
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to group_path(@message.group_id) }
+        format.json { render json: @message }
+      end
     else
       redirect_to group_path(message_params[:group_id]), alert: 'メッセージが入力されていません'
     end
