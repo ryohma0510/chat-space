1371d4c6c830cc264cee0ac77aee48814c91a123
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-04-25 13:24:48 +0900

    modify JS and respond_to

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,10 @@ class MessagesController < ApplicationController
     @message = current_user.messages.new(message_params)
     if @message.save
       respond_to do |format|
+        # これはビュー全体を返すためのやつ
         format.html { redirect_to group_path(@message.group_id) }
-        format.json { render json: @message }
+        # これは全体ビューに差し込むビューを返してくる
+        format.json { render json: message_for_js(@message) }
       end
     else
       redirect_to group_path(message_params[:group_id]), alert: 'メッセージが入力されていません'
@@ -12,6 +14,10 @@ class MessagesController < ApplicationController
   end
 
   private
+
+  def message_for_js(message)
+    {content: message.content, user_name: message.user.name, created_at: message.created_at.strftime('%Y年%m月%d日 %H時%M分')}
+  end
 
   def message_params
     params.require(:message).permit(:content, :group_id)
