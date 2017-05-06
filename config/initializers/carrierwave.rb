27bffb71a999db79710d7f4d10abd365e2483f8f
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-05-06 12:51:09 +0900

    remove 'path_style'

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV['AWS_API_KEY'],
     aws_secret_access_key: ENV['AWS_API_SECRET'],
     region:                ENV['AWS_REGION'],
-    path_style:            true
   }
 
   config.fog_directory  = ENV['AWS_S3_BUCKET']
