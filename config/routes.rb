3e137eee3dc08f51b406ef15a5ce032af051f6b9
Ryoma Kawahara <akahirefriend2008@yahoo.com>
2017-05-07 19:49:05 +0900

    settings for AutoReLoad

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for  :users
   root        'groups#index'
   resources   :groups, only: [:new, :create, :show, :edit, :update]
-  resources   :messages, only: [:create]
+  resources   :messages, only: [:create] do
+    collection do
+      get 'reload'
+    end
+  end
   get         'users/search'  => 'users#search'
 end
